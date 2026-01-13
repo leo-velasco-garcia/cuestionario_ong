7de41af3bfb7cb3ece01dd7801db79b760729f54
clara7227 <clarasalgado27@gmail.com>
2026-01-13 17:59:12 +0100

    added 10 questions more

--- main.js
+++ main.js
@@ -41,6 +41,126 @@ let preguntas = [
     respuestaB: "Intento hacer lo necesario para evitar tensiones inútiles.",
     resultadoB: [0, 0, 0, 1, 0],
   },
+    {
+    pregunta: "Pregunta 6",
+    respuestaA:
+      "Intento evitar situaciones desagradables para mí.",
+    resultadoA: [0, 0, 0, 1, 0],
+    respuestaB: "Intento ganar en mi posición.",
+    resultadoB: [1, 0, 0, 0, 0],
+  },
+    {
+    pregunta: "Pregunta 7",
+    respuestaA:
+      "Intento atrasar el tema hasta que he tenido tiempo para pensarlo.",
+    resultadoA: [0, 0, 0, 1, 0],
+    respuestaB: "Cedo en algunos temas a cambio de otros.",
+    resultadoB: [0, 0, 1, 0, 0],
+  },
+    {
+    pregunta: "Pregunta 8",
+    respuestaA:
+      "Suelo ser dura o duro cuando persigo mis metas.",
+    resultadoA: [1, 0, 0, 0, 0],
+    respuestaB: "Dejaré que gane alguna de sus posiciones si me deja a mí ganar alguna de las mías.",
+    resultadoB: [0, 1, 0, 0, 0],
+  },
+    {
+    pregunta: "Pregunta 9",
+    respuestaA:
+      "Creo que hay veces que no merece la pena preocuparse por las diferencias.",
+    resultadoA: [0, 0, 0, 1, 0],
+    respuestaB: "Hago bastantes esfuerzos por lograr lo que quiero.",
+    resultadoB: [1, 0, 0, 0, 0],
+  },
+    {
+    pregunta: "Pregunta 10",
+    respuestaA:
+      "Soy dura o duro cuando persigo mis metas.",
+    resultadoA: [1, 0, 0, 0, 0],
+    respuestaB: "Trato de encontrar una solución equilibrada para ambas partes.",
+    resultadoB: [0, 0, 1, 0, 0],
+  },
+     {
+    pregunta: "Pregunta 11",
+    respuestaA:
+      "Dejaré que gane alguna de sus posiciones si me deja a mí ganar alguna de las mías.",
+    resultadoA: [0, 1, 0, 0, 0],
+    respuestaB: "Quizá intentaría calmar los sentimientos de la otra parte y conservar la relación.",
+    resultadoB: [0, 0, 0, 0, 1],
+  },
+     {
+    pregunta: "Pregunta 12",
+    respuestaA:
+      "Algunas veces evito tomar posiciones que puedan crear conflicto.",
+    resultadoA: [0, 0, 1, 0, 0],
+    respuestaB: "Dejaré que gane alguna de sus posiciones si me deja a mí ganar alguna de las mías.",
+    resultadoB: [0, 0, 0, 1, 0],
+  },
+     {
+    pregunta: "Pregunta 13",
+    respuestaA:
+      "Yo propongo un punto intermedio.",
+    resultadoA: [0, 0, 1, 0, 0],
+    respuestaB: "Yo empujo para establecer mis puntos.",
+    resultadoB: [1, 0, 0, 0, 0],
+  },
+     {
+    pregunta: "Pregunta 14",
+    respuestaA:
+      "Yo le digo mis ideas y le pido las suyas.",
+    resultadoA: [0, 1, 0, 0, 0],
+    respuestaB: "Intento enseñarle los beneficios y lo lógico de mi posición.",
+    resultadoB: [1, 0, 0, 0, 0],
+  },
+     {
+    pregunta: "Pregunta 15",
+    respuestaA:
+      "Quizá intentaría calmar los sentimientos de la otra parte y conservar la relación.",
+    resultadoA: [0, 0, 0, 0, 1],
+    respuestaB: "Trato de hacer lo necesario para evitar la tensión.",
+    resultadoB: [0, 0, 0, 1, 0],
+  },
+     {
+    pregunta: "Pregunta 16",
+    respuestaA:
+      "Intento no herir los sentimientos de la otra persona.",
+    resultadoA: [0, 0, 0, 0, 1],
+    respuestaB: "Intento convencer a la otra persona de los méritos de mi posición.",
+    resultadoB: [1, 0, 0, 0, 0],
+  },
+     {
+    pregunta: "Pregunta 17",
+    respuestaA:
+      "Suelo ser dura o duro cuando persigo mis metas.",
+    resultadoA: [1, 0, 0, 0, 0],
+    respuestaB: "Intento hacer lo necesario para evitar tensiones inútiles.",
+    resultadoB: [0, 0, 0, 1, 0],
+  },
+     {
+    pregunta: "Pregunta 18",
+    respuestaA:
+      "Si le hace feliz a la otra parte, quizá la dejo mantener sus ideas.",
+    resultadoA: [0, 0, 0, 0, 1],
+    respuestaB: "Dejaré que gane alguna de sus posiciones si me deja a mí ganar alguna de las mías.",
+    resultadoB: [0, 0, 1, 0, 0],
+  },
+     {
+    pregunta: "Pregunta 19",
+    respuestaA:
+      "Dejaré que gane alguna de sus posiciones si me deja a mí ganar alguna de las mías.",
+    resultadoA: [0, 1, 0, 0, 0],
+    respuestaB: "Intento atrasar el tema hasta que he tenido tiempo para pensarlo.",
+    resultadoB: [0, 0, 0, 1, 0],
+  },
+     {
+    pregunta: "Pregunta 20",
+    respuestaA:
+      "Intento trabajar los problemas inmediatamente.",
+    resultadoA: [0, 1, 0, 0, 0],
+    respuestaB: "Intento encontrar una contribución justa de pérdidas y ganancias igualmente para mí y para la otra parte.",
+    resultadoB: [0, 0, 1, 0, 0],
+  },
 ];
 
 let valoresFinales = [
@@ -116,7 +236,13 @@ const guardarRespuesta = () => {
         valoresFinales[j] += preguntas[i][respuesta[i]][j];
       }
 
-      resultados.innerHTML = `Competición ${valoresFinales[0]}, colaboración ${valoresFinales[1]}, compartir ${valoresFinales[2]}, evitación ${valoresFinales[3]}, acomodación ${valoresFinales[4]}`;
+      resultados.innerHTML = `<ul>
+      <li>Competición ${valoresFinales[0]}</li>
+      <li>Colaboración ${valoresFinales[1]}</li>
+      <li>Compartir ${valoresFinales[2]}</li>
+      <li>Evitación ${valoresFinales[3]}</li>
+      <li>Acomodación ${valoresFinales[4]}</li>
+      </ul>`;
     }
     form.reset();
     respuesta = [];
@@ -148,3 +274,6 @@ boton.addEventListener("click", function (event) {
 });
 
 let resultados = document.querySelector("#resultados");
+
+// añadir clase al elemento de resultados de "hidden" o "visible" según si se han contestado las respuestas,
+// y un "close".
